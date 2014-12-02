ae918364a054ed8453a96a405646d897bd0cd692
Aleksey Izmailov <izmailoff@gmail.com>
2014-12-02 16:32:00 +0800

    Connected tshark process to Scala app in non-blocking matter. Each event will be consumed by an actor. WIP.

--- src/main/scala/com/wp/boot/Boot.scala
+++ src/main/scala/com/wp/boot/Boot.scala
@@ -3,6 +3,8 @@ package com.wp.boot
 import akka.actor.{Actor, ActorSystem, Props}
 import akka.kernel.Bootable
 
+import scala.sys.process.Process
+
 case object Start
 
 class HelloActor extends Actor {
@@ -11,7 +13,7 @@ class HelloActor extends Actor {
   def receive = {
     case Start => worldActor ! "Hello"
     case message: String =>
-      println("Received message '%s'" format message)
+      println(s"Received message [$message].")
   }
 }
 
@@ -22,15 +24,38 @@ class WorldActor extends Actor {
 }
 
 class Boot extends Bootable {
-  val system = ActorSystem("hellokernel")
+  val system = ActorSystem("wifi_presense_kernel")
+
+  lazy val eventConsumer = system.actorOf(Props[HelloActor])
 
   def startup = {
-    system.actorOf(Props[HelloActor]) ! Start
+    eventConsumer ! Start
   }
 
   def shutdown = {
     system.terminate()
   }
+
+
+  //val cmd = //"ls"
+    //"""stdbuf -oL tshark -i wlp3s0 -I -f 'broadcast' -Y 'wlan.fc.type == 0 && wlan.fc.subtype == 4' -T fields -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal"""
+  //val tsharkCmd = """/usr/sbin/tshark -i wlp3s0 -I -f 'broadcast' -Y 'wlan.fc.type == 0 && wlan.fc.subtype == 4' -T fields -e frame.time_epoch -e wlan.sa -e radiotap.dbm_antsignal"""
+  val cmdArgs = Seq("/usr/sbin/tshark", "-iwlp3s0", "-I",
+  "-fbroadcast",  "-Tfields", "-eframe.time_epoch", //"-Y'wlan.fc.type == 0 && wlan.fc.subtype == 4'",
+  "-ewlan.sa", "-eradiotap.dbm_antsignal") //"stdbuf", "-oL",
+  println(cmdArgs)
+  val pb = Process(cmdArgs)
+  println(pb.toString)
+  //Thread.sleep(20000)
+  //linesStream foreach (line => eventConsumer ! line)
+
+  import scala.sys.process.ProcessIO
+  val pio = new ProcessIO(_ => (),
+    stdout => scala.io.Source.fromInputStream(stdout).getLines.foreach(line => eventConsumer ! line),
+    stderr => scala.io.Source.fromInputStream(stderr).getLines.foreach(line => eventConsumer ! "ERR:" + line))
+  pb.run(pio)
+
+  println("FINISHED!!!!!!!!!!!!")
 }
 
 
